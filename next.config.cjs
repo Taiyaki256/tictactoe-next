170bfdebce79f68f0379308dff4d97480ba828fa
Taiyaki256 <taiyakey@mineo.jp>
2024-05-06 14:25:04 +0900

    fix

--- next.config.cjs
+++ next.config.cjs
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const nextConfig = {
+    export:"./out",
     webpack(config, options) {
         config.resolve.alias['@'] = path.join(__dirname, 'src')
         return config;
