ac38e5071c518c7f37dc76230281ee57803d4377
Taiyaki256 <taiyakey@mineo.jp>
2024-05-06 23:29:21 +0900

    remove console

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@
 // import { path } from 'node:path'
 const nextConfig = {
     output: 'export',
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production',
+    }
     // webpack(config, options) {
     //     config.resolve.alias['@'] = path.join(__dirname, 'src')
     //     return config;
