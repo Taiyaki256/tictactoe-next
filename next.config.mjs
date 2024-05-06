67da381933b912f98e35c7a161a1bddf0feb6f38
Taiyaki256 <taiyakey@mineo.jp>
2024-05-06 15:04:05 +0900

    fix for build

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+// import { path } from 'node:path'
+const nextConfig = {
+    output: 'export',
+    // webpack(config, options) {
+    //     config.resolve.alias['@'] = path.join(__dirname, 'src')
+    //     return config;
+    //   },
+};
+
+export default nextConfig;
